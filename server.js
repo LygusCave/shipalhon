4d00c22202c6fb5fc8d3d42caaae68c1cd16ceaa
LygusCave <shmyga3000@gmail.com>
2026-03-25 23:15:25 +0300

    from port to socket

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import {toPallad, cyclePinyinPall, capitalizeFirstLetter} from './utils/converter/cnToPall.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = 10000;
+const SOCKET_PATH = path.join(__dirname, 'server.sock');
 const jsonParser = express.json();
 
 app.use((req, res, next) => {
@@ -27,8 +28,12 @@ app.post("/transcriptCN", jsonParser, function (request, response) {
     const ruText = cyclePinyinPall(responseText, text.spaceBool,text.erhuaBool);
     response.send(ruText);
 });
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущен! Перейди: http://localhost:${PORT}`);
+if (fs.existsSync(SOCKET_PATH)) {
+    fs.unlinkSync(SOCKET_PATH);
+}
+app.listen(SOCKET_PATH, () => {
+    fs.chmodSync(SOCKET_PATH, '0666'); 
+    
+    console.log(`Сервер запущен через сокет: ${SOCKET_PATH}`);
     console.log("时：", new Date().toLocaleString());
 });
