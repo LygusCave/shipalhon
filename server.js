08cc04a08a84c24fd8280d4cdbd97a1ab8cea72b
LygusCave <shmyga3000@gmail.com>
2026-04-19 04:32:50 +0300

    quick fix

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use((req, res, next) => {
     console.log(`Пришел запрос: ${req.method} ${req.url}`);
     next();
 });
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist'), {
+    extensions: ['html', 'htm'],
+    index: 'index.html'
+}));
 app.use(express.static(path.join(__dirname, 'front')));
 // Обработка конкретных путей в папке dist
 app.get('/article/:name', (req, res) => {
@@ -29,13 +32,20 @@ app.get('/article/:name', (req, res) => {
 
 // Если вы планируете сделать SPA (Single Page Application) в будущем, 
 // можно добавить этот костыль для всех GET запросов:
-app.get('*', (req, res) => {
-    // Проверяем, есть ли такой файл в dist/название/index.html
-    const fallbackPath = path.join(__dirname, 'dist', req.path, 'index.html');
+app.get('/*', (req, res, next) => {
+    if (req.path === '/') return next();
+
+    // Исправляем формирование пути, убирая лишние слеши
+    const cleanPath = req.path.replace(/^\/+/, '');
+    const fallbackPath = path.resolve(__dirname, 'dist', cleanPath, 'index.html');
+    
+    console.log(`[Debug] Пробуем найти файл: ${fallbackPath}`);
+
     if (fs.existsSync(fallbackPath)) {
         return res.sendFile(fallbackPath);
     }
-    res.status(404).send('Страница не найдена');
+    
+    res.status(404).send(`Страница ${req.path} не найдена`);
 });
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'front', 'index.html'));
